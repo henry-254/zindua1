a48f19ed84e91478ccea7f2ba3a4aaf0b4c99de9
henry-254 <henkamwash@gmail.com>
2023-02-18 13:03:47 +0300

    add operation mult and sub

--- hello.py
+++ hello.py
@@ -2,8 +2,11 @@ name = " hello world"
 number = 2
 number_1 = 3
 add = number + number_1
+mult = number * number_1
+sub = number - number_1
 
 print(name)
 print(number)
 print(add)
-
+print(mult)
+print(sub)
